0d033561afa205e4aff60b35d8bc500d2022d07f
MorasDeFresa <bencito10@gmail.com>
2025-11-07 11:56:27 -0500

    🐞 fix: Width size

--- components/BooksTable.tsx
+++ components/BooksTable.tsx
@@ -6,8 +6,9 @@ const BooksTable = ({ books }) => {
     <View>
       {/* Cabecera de la tabla */}
       <View className="flex-row bg-gray-200 py-2 px-1">
-        <Text className="flex-1 font-semibold text-center text-3xl w-1/2">Titulo</Text>
-        <Text className="flex-1 font-semibold text-center text-3xl w-1/2">Descripción</Text>
+        <Text className="w-1/6 font-semibold text-2xl">Id</Text>
+        <Text className="w-2/6 font-semibold text-center text-2xl">Titulo</Text>
+        <Text className="w-3/6 font-semibold text-right text-2xl pr-2">Descripción</Text>
       </View>
 
       <FlatList
@@ -19,9 +20,12 @@ const BooksTable = ({ books }) => {
           <TouchableHighlight onPress={() => { Alert.alert('You long-pressed the button!'); }} underlayColor="white">
             <View className="flex-row py-7 border-b">
               <Text className="flex-1 px-1 text-black text-3xl">
+                {`${item.userId}${item.id}`}
+              </Text>
+              <Text className="flex-1 px-1 text-black text-3xl">
                 {item.title.length > 5 ? `${item.title.slice(0, 5)}…` : item.title}
               </Text>
-              <Text className="flex-1 px-7 text-black text-2xl">
+              <Text className="flex-1 px-1 text-black text-2xl">
                 {item.body.length > 10 ? `${item.body.slice(0, 10)}…` : item.body}
               </Text>
             </View>
